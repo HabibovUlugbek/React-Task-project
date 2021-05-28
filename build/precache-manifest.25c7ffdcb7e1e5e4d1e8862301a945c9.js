8a647c8b749590a9c38771e19722d343f90d5e68
HabibovUlugbek <72121629+HabibovUlugbek@users.noreply.github.com>
2021-05-28 17:10:27 +0500

    Add files via upload
    build app for deploy

---
+++ build/precache-manifest.25c7ffdcb7e1e5e4d1e8862301a945c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab5164f9e2b213a6b0b68d85c37bf404",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16fce09dc416d4d849be",
+    "url": "/static/css/main.e6bb01d6.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16fce09dc416d4d849be",
+    "url": "/static/js/main.ee46e633.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
